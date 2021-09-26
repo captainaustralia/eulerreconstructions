779ff5e9cd59fba8a3b008c8d5169e19000eb559
mwmwm <61281668+physrow@users.noreply.github.com>
2021-09-26 17:22:40 +0700

    upd

--- Euler reconstruction.py
+++ Euler reconstruction.py
@@ -1,23 +1,19 @@
 import math
 
-from sympy import diff, symbols
-from sympy.abc import x, y
+from sympy import diff, symbols, det, Matrix
+from sympy.abc import x, y, d
+import numpy
 
-""" Необходимо реализовать алгоритм , который восстанавливает исходную форму уравнения Эйлера, по заданным корням
-характеристического уравнения . Алгоритм разделяется на следующие шаги:
-1. Необходимо проанализировать корни:
-1.1 Корни действительные => подставим их в формулу y = x^k , где k - корень хар.ур-ия
-1.2 Корни комплексные => значит переводим их в соответствующую форму (cos/tan/sin etc)
-2. Каждый n - корень, формирует n - решение, т.е
-y_n(1), y_n(2) , y_n(3)
-Из данных корней необходимо составить фундаментальную систему решений - ФСР
-| y    y'    y''  y''' ....|
-| x^k x^k' x^k'' x^k'''    |
- ..........................|
-Таким образом получаем опр.Вронского, раскрытие которого приведёт к исх. ур-ию"""
+a = {str(i): symbols("y" + "'" * i) for i in range(50)}
 
+funcs = {str(i): "y" + "'" * i for i in range(20)}
 matrix = []
 first_row = []
+
+
+def draw_matrix(array):
+    for i in array():
+        print(i)
 
 
 def find_determinate(order, array):
@@ -26,10 +22,10 @@ def find_determinate(order, array):
     new_row = []
     result = []
     string_result = ''
-    if order == 1:
+    if order == 1:  # matrix 2 order
         print(str(array[0][0]) + '*' + str(array[1][1]) + ' - ' + str(array[0][1]) + '*' + str(array[1][0]) + ' = 0')
         return str(array[0][0]) + '*' + str(array[1][1]) + ' - ' + str(array[0][1]) + '*' + str(array[1][0]) + ' = 0'
-    if order == 2:
+    if order == 2:  # matrix 3 order _| method with minors
         for i in range(order + 1):
             for j in range(order):
                 for k in range(order + 1):
@@ -42,36 +38,39 @@ def find_determinate(order, array):
             result.append(new_matrix[0][0] * new_matrix[1][1] - new_matrix[0][1] * new_matrix[1][0])
             new_matrix = []
         for i in range(len(result)):
-            string_result += str(result[i]) + 'y' + "'" * i + ' '
-        print(string_result)
+            if i % 2 == 0 and len(result) - 1 != i:
+                string_result += 'y' + "'" * i + '*' + '(' + str(result[i]) + ') + '
+            elif i % 2 != 0 and i != len(result) - 1:
+                string_result += '( - y' + "'" * i + ')' + '*' + '(' + str(result[i]) + ') + '
+            else:
+                string_result += '( - y' + "'" * i + ')' + '*' + '(' + str(result[i]) + ') = 0 or ...'
+        print(string_result.replace('**', '^'))
         return string_result
-    if order == 3:
-        for i in range(order + 1):
-            for j in range(order):
-                for k in range(order + 1):
-                    if i == k:
-                        pass
-                    else:
-                        new_row.append(matrix[j][k])
-                new_matrix.append(new_row)
-                new_row = []
-        for i in new_matrix:
-            print(i)
+    if order >= 3:  # matrix >= 4 order ...
+        matrix = Matrix(array)
+        print("\t" * 20 + "EULER FORM:")
+        print("_" * 250)
+        print(str(det(matrix)) + " = 0 or ....")
 
 
-def find_euler(order=int(input('Введите порядок уравнения: '))):
+def find_euler(order=int(input('Enter the order of the equation: '))):
     # порядок уравнения соответствует количеству решений
+    visual_row = '\t' * 20 + 'fundamental decision system'.upper() + '\n'
     row, col = order, order
-    print(row, col)
-    roots = list(map(int, input('Введите характеристические корни(через пробел): ').split()))
+    roots = list(map(int, input('Enter characteristic roots (separated by a space): ').split()))
     roots = [x ** i for i in roots]
-    print(roots, ' Подстановка характеристических корней по формуле y = x^k \n\n')
+    print(roots, ' Substitution of characteristic roots by the formula y = x^k \n\n')
     if len(roots) != order:
-        print('Количество корней не соответствует порядку уравнения.\nневозможно построить ФСР')
-        return find_euler(int(input('\nВведите порядок уравнения: ')))
+        print(
+            'The number of roots does not match the order of the equation'
+            '\nImpossible to build FDS\n'
+            'Unable to compute determinant'
+        )
+        return find_euler(int(input('\nEnter the order of the equation: ')))
     else:
         for i in range(order + 1):
-            first_row.append('y' + "'" * i)
+            # first_row.append('y' + "'" * i)
+            first_row.append(a[str(i)])
         matrix.append(first_row)
         for i in range(len(roots)):
             differentiated_roots = []
@@ -82,8 +81,15 @@ def find_euler(order=int(input('Введите порядок уравнения
                 if len(differentiated_roots) == order:
                     differentiated_roots.insert(0, roots[i])
                     matrix.append(differentiated_roots)
-        for i in matrix:
-            print(i)
+        for i in range(order):
+            for j in range(order):
+                space = 15
+                if len(str(matrix[i][j])) > 1:
+                    space = space - (len(str(matrix[i][j])) - 2)
+                visual_row += "|    " + str(matrix[i][j]).replace("**", '^').replace("*", '') + " " * space + '|'
+            visual_row += '\n'
+        print(visual_row)
+
         find_determinate(order, matrix)
 
 
